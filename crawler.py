f12fcbe8e3923adc21e4bb5ff4257200df104baf
Utkarsh S. <utkarshsolanki776@gmail.com>
2026-08-29 02:16:14 +0530

    rename crawl helper to main_crawl, add text dump mode

--- crawler.py
+++ crawler.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 
 
-def crawl(name, html):
-    soup = BeautifulSoup(html, "html.parser")
+def main_crawl(r,text=None):
+    soup = BeautifulSoup(r.text, "html.parser")
     for link in soup.find_all("a"):
         href = link.get("href")
         if href:
-            print(f"link in {name}: {href}")
+            print(f"link in {r.url}: {href}")
+        if text:
+            all_text = soup.get_text(" ", strip=True)
+            print(f"text content of {r.url} , {all_text} ")
